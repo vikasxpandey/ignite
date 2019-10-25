4484941f764653643b3c4268130d18c2037241ca
Vikas Pandey <51741443+vikasxpandey@users.noreply.github.com>
2019-10-26 01:39:03 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://vikas:vikas@mernconnect-q9aqk.mongodb.net/test?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
+  mongoURI: '',      // Mongo URI
+  secretOrKey: ''    // Mongo Secret
 };
